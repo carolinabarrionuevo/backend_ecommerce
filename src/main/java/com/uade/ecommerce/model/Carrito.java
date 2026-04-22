ec43aea980e0f5a0102bc60b50899bb1d870f8d1
sbevitore <sbevitore@uade.edu.ar>
2026-04-22 13:03:47 -0300

    add entidad Carrito

---
+++ src/main/java/com/uade/ecommerce/model/Carrito.java
@@ -0,0 +1,29 @@
+package com.uade.ecommerce.model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Carrito {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_carrito") 
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    @OneToMany(mappedBy = "carrito")
+    private List<ItemCarrito> items;
+
+
+}
